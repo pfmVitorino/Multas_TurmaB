13e638ea452f115b78c601e427328b0e584a97f5
Pedro vitorino <PedroVitorino04@hotmail.com>
2019-04-30 16:40:42 +0100

    Adicionar anotadores para formatar o modelo dos agentes.

--- MultasB/MultasB/Models/Agentes.cs
+++ MultasB/MultasB/Models/Agentes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,22 @@ namespace MultasB.Models
 {
     public class Agentes
     {
-
+        [Key] // identificação do atributo como primary key
         public int ID { get; set; }
 
+        [Required (ErrorMessage ="O Nome é de preenchimento obrigatório")]
+        [StringLength(50,ErrorMessage ="O {0} deve ter, no máximo, {1} caracteres")]
+        [RegularExpression("ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+ [A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+(( | '|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-z] +[a-záéíóúàèìòùâêîôûäëïöüãõç]+[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+){1, 3}",
+            ErrorMessage ="O {0} so pode conter letras. Cada palavra deve começar por maiúscula.")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "A esquadra é de preenchimento obrigatório")]
+        [StringLength(20, ErrorMessage = "A {0} deve ter, no máximo, {1} caracteres")]
+        [RegularExpression("ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+ [A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+(( | '|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-z] +[a-záéíóúàèìòùâêîôûäëïöüãõç]+[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+){1, 3}",
+            ErrorMessage = "A {0} so pode conter letras. Cada palavra deve começar por maiúscula.")]
         public string Esquadra { get; set; }
 
+        
         public string Fotografia { get; set; }
 
 
